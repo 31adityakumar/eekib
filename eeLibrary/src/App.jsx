c238f3a77cb4e22b45175a681c65facbba564ce4
Devansh Pandey <devanshpandey2003dp@gmail.com>
2024-07-30 23:49:57 +0530

    ADD: Material Page

--- eeLibrary/src/App.jsx
+++ eeLibrary/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
 import Messages from "./components/Messages/Messages";
+import Material from "./Pages/Material/Material";
 function App() {
   return (
     <>
@@ -10,6 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} key="route-home-screen" />
+          <Route exact path="/material" element={<Material />} key="route-home-screen" />
           <Route exact path="/messages" element={<Messages/>} key="route-home-screen" />
         </Routes>
         <Footer />
---
+++ eeLibrary/src/Pages/Material/Material.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "./Material.module.scss"
+const Material = () => {
+  return (
+    <div className={styles.material}>
+        Material
+    </div>
+  )
+}
+
+export default Material
